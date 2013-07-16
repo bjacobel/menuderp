08447e61de199142eb5fd478a8bf0047f02be7bd
Brian Jacobel <bjacobel@theatlantic.com>
2013-07-15 20:33:35 -0400

    D,DT causing import errors

--- menuderp/settings/dev.py
+++ menuderp/settings/dev.py
@@ -55,7 +55,7 @@ CELERY_ALWAYS_EAGER = True
 ########## TOOLBAR CONFIGURATION
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 INSTALLED_APPS += (
-    'debug_toolbar',
+    #'debug_toolbar',
 )
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
@@ -63,6 +63,6 @@ INTERNAL_IPS = ('127.0.0.1',)
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 ########## END TOOLBAR CONFIGURATION
