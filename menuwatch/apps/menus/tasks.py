e5405a12135609ca8e32d6dee5b0b7976aa8dc9f
Brian Jacobel <bjacobel@theatlantic.com>
2013-07-18 21:05:03 -0400

    Fuck PHP

---
+++ menuwatch/apps/menus/tasks.py
@@ -0,0 +1,10 @@
+from celery import task
+from datetime import datetime
+import requests
+
+@task()
+def build_db():
+    # Grab the next week's worth of food and add it to the database.
+    # Test for success, then delete the last week's worth. 
+
+
