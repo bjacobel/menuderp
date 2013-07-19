b93c9ac7b3a4a727a7867939b0beaee19e054500
Brian Jacobel <bjacobel@theatlantic.com>
2013-07-18 21:08:28 -0400

    almost there on hte parser

--- build_db.py
+++ build_db.py
@@ -8,7 +8,7 @@ locations = {
     "Thorne": 49,
 }
 
-if today.weekday() is (5 or 6):
+if today.isoweekday() is (6 or 7):
     meals_available_today = ["Brunch", "Dinner"]
 else:
     meals_available_today = ["Breakfast", "Lunch", "Dinner"]
@@ -32,18 +32,27 @@ for key, value in locations.iteritems():
             # remove <br>, \n, </h3>, </span>, </html>, </body>s
             html = re.sub(r'\n|<br>|</h3>|</span>|</html>|</body>', "", html)
 
+            # their utf-8 is broken
+            html = re.sub(r'&amp;', '&', html)
+
             # remove everything up to the first header
             html = re.split(r'<h3>', html)
             html.pop(0)
 
             # fuck express meal
             for meal in html:
-                if re.search("Express Meal", meal):
-                    html.remove(meal)
+                if !re.search("Express Meal", meal):
 
-                # grab the food group (main course, soup, vegetable, etc)
-                foodgroup = re.split('<span>', meal)[0]
+                    # grab the food group (main course, soup, vegetable, etc)
+                    foodgroup = re.split('<span>', meal)[0]
 
-                foods = re.split('<span>', meal)[1:]
-                for food in foods:
-                    
+                    foods = re.split('<span>', meal)[1:]
+                    for food in foods:
+
+                        # grab the food details (ve, gf)
+                        deets = []
+                        match = re.search(r'\(.+\)', food)
+                        if match:
+                            deets = match.group()
+
+                        #addRow(today.year, today.month, today.day, today.isoweekday(), value, meal)
