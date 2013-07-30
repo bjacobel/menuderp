8e199789fd37d97e6cfb177d8fafd5405c1548cf
Brian Jacobel <bjacobel@theatlantic.com>
2013-07-30 17:41:19 -0400

    fu travis

--- pathfix.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-base = os.path.dirname(os.path.dirname("menuwatch/settings/prod.py"))
-base_parent = os.path.dirname(base)
-sys.path.append(base)
-sys.path.append(base_parent)
-print(":".join(sys.path))
