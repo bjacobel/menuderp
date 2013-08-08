faf828551e709e60b58ae0563e4ceb7cb56d3717
Brian Jacobel <bjacobel@theatlantic.com>
2013-08-07 20:28:28 -0400

    Initial gh-pages commit

---
+++ pelican/pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Menuwatch'
+SITENAME = u'News | Menuwatch'
+SITEURL = ''
+
+THEME = u'themes/pelican-svbtle/'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Menuwatch @ Twitter', 'http://twitter.com/menuwatch'),
+          ('Menuwatch @ GitHub', 'http://github.com/bjacobel/menuwatch'),
+          ('bjacobel @ Twitter', 'http://twitter.com/bjacobel'),
+          ('bjacobel @ GitHub', 'http://github.com/bjacobel'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
