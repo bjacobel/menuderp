080652751390c18f6aa97cd608fefcdc7aea7323
Brian Jacobel <bjacobel@gmail.com>
2013-08-16 18:58:07 -0500

    Moving logo to outside project

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -33,5 +33,7 @@ GOOGLE_ANALYTICS = u'UA-38327978-2'
 
 AUTHOR_BIO = 'A website designed around the Bowdoin dining experience. <a href="http://www.menuwat.ch">menuwat.ch</a>'
 
+LOGO = u'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="80px" height="80px" viewBox="-6.05 -16.73 80 80" enable-background="new -6.05 -16.73 80 80" xml:space="preserve"><g><circle fill="#918A8A" cx="35.7" cy="17" r="26"/><g><g><polygon fill="#E67E22" points="29.5,5 41.4,28.8 53.3,5"/></g></g><g><g><polygon fill="#3498DB" points="29.6,5 17.8,28.8 41.5,28.8"/></g></g></g></svg>'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
