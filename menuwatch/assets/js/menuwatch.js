00538b630ae43422e9438eee1b23380169779bad
Brian Jacobel <bjacobel@gmail.com>
2013-08-25 11:53:01 -0500

    rejiggering of templated js blocks (now featuring django-compressor)

--- menuwatch/assets/js/menuwatch.js
+++ menuwatch/assets/js/menuwatch.js
@@ -1,3 +1,8 @@
+/*  
+    ONLY SITE-WIDE SCRIPTS go in this file. Anything else should go into
+    {% footer_scripts %} blocks in the views it needs to run on.
+*/
+
 $(document).ready(function() {
     // set focus on first text field of any page
      $("input:text:visible:first").focus();
