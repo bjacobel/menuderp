97c3a911cd6647b7eb95ec82f01632cd80c0b586
Brian Jacobel <bjacobel@gmail.com>
2013-09-05 14:30:22 -0400

    redisifying

--- menuwatch/settings/prod.py
+++ menuwatch/settings/prod.py
@@ -22,28 +22,17 @@ CACHES = memcacheify()
 
 ########## CELERY CONFIGURATION
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-transport
-BROKER_TRANSPORT = 'amqplib'
-
-# Set this number to the amount of allowed concurrent connections on your AMQP
-# provider, divided by the amount of active workers you have.
-#
-# For example, if you have the 'Little Lemur' CloudAMQP plan (their free tier),
-# they allow 3 concurrent connections. So if you run a single worker, you'd
-# want this number to be 3. If you had 3 workers running, you'd lower this
-# number to 1, since 3 workers each maintaining one open connection = 3
-# connections total.
-#
-# See: http://docs.celeryproject.org/en/latest/configuration.html#broker-pool-limit
-BROKER_POOL_LIMIT = 1
-
-# See: http://docs.celeryproject.org/en/latest/configuration.html#broker-connection-max-retries
-BROKER_CONNECTION_MAX_RETRIES = 0
+BROKER_TRANSPORT = 'redis'
 
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-url
-BROKER_URL = environ.get('RABBITMQ_URL') or environ.get('CLOUDAMQP_URL')
+BROKER_URL = environ.get('REDIS_URL')
+
+CELERY_REDIS_MAX_CONNECTIONS = 9
+
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 
 # See: http://docs.celeryproject.org/en/latest/configuration.html#celery-result-backend
-CELERY_RESULT_BACKEND = 'amqp'
+CELERY_RESULT_BACKEND = BROKER_URL
 ########## END CELERY CONFIGURATION
 
 ########## SECRET CONFIGURATION
