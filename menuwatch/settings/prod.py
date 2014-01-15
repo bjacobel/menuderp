abfad9886fc607da7c08f077295bc38643672851
Brian Jacobel <bjacobel@gmail.com>
2014-01-15 16:39:35 -0600

    Switched back RabbitMQ BigWig - Redis was running out of connections and CloudAMQ is crippled

--- menuwatch/settings/prod.py
+++ menuwatch/settings/prod.py
@@ -14,25 +14,30 @@ DATABASES = postgresify()
 ########## END DATABASE CONFIGURATION
 
 
-########## CACHE CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = memcacheify()
-########## END CACHE CONFIGURATION
-
-
 ########## CELERY CONFIGURATION
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-transport
-BROKER_TRANSPORT = 'redis'
+BROKER_TRANSPORT = 'amqplib'
+
+# Set this number to the amount of allowed concurrent connections on your AMQP
+# provider, divided by the amount of active workers you have.
+#
+# For example, if you have the 'Little Lemur' CloudAMQP plan (their free tier),
+# they allow 3 concurrent connections. So if you run a single worker, you'd
+# want this number to be 3. If you had 3 workers running, you'd lower this
+# number to 1, since 3 workers each maintaining one open connection = 3
+# connections total.
+#
+# See: http://docs.celeryproject.org/en/latest/configuration.html#broker-pool-limit
+BROKER_POOL_LIMIT = 1
+
+# See: http://docs.celeryproject.org/en/latest/configuration.html#broker-connection-max-retries
+BROKER_CONNECTION_MAX_RETRIES = 0
 
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-url
-BROKER_URL = environ.get('REDIS_URL')
-
-CELERY_REDIS_MAX_CONNECTIONS = 9
-
-BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+BROKER_URL = environ.get('RABBITMQ_BIGWIG_TX_URL')
 
 # See: http://docs.celeryproject.org/en/latest/configuration.html#celery-result-backend
-CELERY_RESULT_BACKEND = BROKER_URL
+CELERY_RESULT_BACKEND = 'amqp'
 ########## END CELERY CONFIGURATION
 
 ########## SECRET CONFIGURATION
