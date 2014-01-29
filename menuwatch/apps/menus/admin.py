9ef1309fad56ce4793b58f803ed345008d7998c0
Brian Jacobel <bjacobel@gmail.com>
2014-01-28 23:35:41 -0500

    Added an admin filter for food with no upcoming dates

--- menuwatch/apps/menus/admin.py
+++ menuwatch/apps/menus/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 from models import Food, Watch, Profile
 
+
+# classes for filtering FoodsAdmin
+class UpcomingDatesFilter(SimpleListFilter):
+    title = ('Has Upcoming Dates?')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'hasupcoming'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('0', ('False')),
+            ('1', ('True')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '0':
+            return queryset.filter(next_dates__exact=None)
+        return queryset.exclude(next_dates__exact=None)
 
 class FoodsAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -9,7 +35,7 @@ class FoodsAdmin(admin.ModelAdmin):
         ('More',       {'fields': ['last_date']}),
     ]
     list_display = ('name', 'meal', 'location', 'foodgroup', 'attrs', 'num_watches', 'last_date', 'peek_next_date')
-    list_filter = ['meal', 'foodgroup', 'location', 'attrs']
+    list_filter = ['meal', 'foodgroup', 'location', 'attrs', UpcomingDatesFilter]
     search_fields = ['name']
 
 admin.site.register(Food, FoodsAdmin)
